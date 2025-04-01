14a9add64a9fe8f73be3dd2d208ef9774f3a7953
athulZartek <athul.vb@zartek.in>
2025-04-01 11:30:22 +0530

    add shell app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+// import LoginForm from "./Components/LoginComponent";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import ForgotPassword from "./pages/forgot-password";
+// import SignUpPage from "./pages/sign-up";
+// import IdVerification from "./pages/id-verification";
+
+function App() {
+  return (
+    <div>
+      Hello
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/id-verification" element={<IdVerification />} />
+        </Routes>
+      </BrowserRouter> */}
+    </div>
+  );
+}
+
+export default App;
